f40954af57ca4219a7483e826da4bcaab393d7e4
manami01 <s13518mt@sfc.keio.ac.jp>
2014-05-05 17:57:25 +0900

    授業課題　javaに注をつけました

--- js/annimate.js
+++ js/annimate.js
@@ -1,12 +1,14 @@
-var endFlipFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
-	foxkeh.removeEventListener("animationend", endFlipFoxkeh);
+var endFlipFoxkeh = function(){ //この下はふぉくすけのフリップを終了させる動きの一連
+	var foxkeh = document.querySelector("#slidein-foxkeh img");　//ここでslidein-foxkeh imgの中からイメージ（画像、この場合はふぉくすけの画像）の選択をしている
+	foxkeh.setAttribute("class", "slidein");
+	foxkeh.removeEventListener("animationend", endFlipFoxkeh); //removeEventListenerとは登録されているイベントをなくすこと。ここではendFlipFoxkeh（ふぉくすけのフリップを終了させる動きの一連）をなくしている
 
 	foxkeh.setAttribute("class", "");
 };
 
-var endSlideinFoxkehAndStartFlipFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
+var endSlideinFoxkehAndStartFlipFoxkeh = function(){ //この下は「ふぉくすけのスライドを終了させる動きの一連」と「ふぉくすけのフリップを開始する動きの一連」
+	var foxkeh = document.querySelector("#slidein-foxkeh img");　//ここでslidein-foxkeh imgの中からイメージ（画像、この場合はふぉくすけの画像）の選択をしている
+	foxkeh.setAttribute("class", "slidein");
 
 	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
 	foxkeh.addEventListener("animationend", endFlipFoxkeh);
@@ -14,12 +16,12 @@ var endSlideinFoxkehAndStartFlipFoxkeh = function(){
 	foxkeh.setAttribute("class", "flip");
 };
 
-var startSlideinFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
-	foxkeh.setAttribute("class", "slidein");
+var startSlideinFoxkeh = function(){ //この下はstartSlideinfoxkehに関する説明
+	var foxkeh = document.querySelector("#slidein-foxkeh img"); //ここでslidein-foxkeh imgの中からイメージ（画像、この場合はふぉくすけの画像）の選択をしている
+	foxkeh.setAttribute("class", "slidein"); //
 
-	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh); //新たに「ふぉくすけのスライドを終了させる動きの一連」と「ふぉくすけのフリップを開始する動きの一連」を追加している
 };
 
-var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");　//この下はslideinFockehButtonに関する説明
+slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh); //slideinFockehButtonがクリックされると、startSlideinfoxkehをいう動きが発動する
--- js/slidein.js
+++ js/slidein.js
@@ -1,19 +1,18 @@
-var endSlideinFoxkeh = function(){ //この下はスライドするのをやめる動きの一連
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
+var endSlideinFoxkeh = function(){ //この下はスライドを終了させる動きの一連
+	var foxkeh = document.querySelector("#slidein-foxkeh img");　//ここでslidein-foxkeh imgの中からイメージ（画像、この場合はFふぉくすけの画像）の選択をしている
 <<<<<<< HEAD
-	foxkeh.setAttribute("class", "slidein");
+	foxkeh.setAttribute("class", "slidein"); 
 =======
 	foxkeh.setAttribute("class", "");
 };
 
-var startSlideinFoxkeh = function(){ //この下はスライドする動きの一連
-	var foxkeh = document.querySelector("#slidein-foxkeh img"); //ここでイメージ（画像、この場合はFoくすけの画像を選んでいる）の選択をしている
+var startSlideinFoxkeh = function(){ //この下はスライドを始める動きの一連
+	var foxkeh = document.querySelector("#slidein-foxkeh img"); //ここでslidein-foxkeh imgの中からイメージ（画像、この場合はふぉくすけの画像）の選択をしている
 	foxkeh.setAttribute("class", "slidein");
 
-	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
+	foxkeh.addEventListener("animationend", endSlideinFoxkeh); //新たに「ふぉくすけのスライドを終了させる動きの一連」と「ふぉくすけのフリップを開始する動きの一連」を追加している
 >>>>>>> origin/make_slidein_retriable
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
-//アニメーション開始ボタンをクリックすると、startSlideinFoxekehを発動させる
+slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh); //アニメーション開始ボタンをクリックすると、startSlideinFoxekehを発動させる
